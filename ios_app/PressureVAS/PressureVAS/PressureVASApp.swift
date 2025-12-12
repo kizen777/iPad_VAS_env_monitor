185d5b5bf49fa36666be8862725e0ac260b63cfd
kizen777 <kizen777@gmail.com>
2025-12-12 17:43:24 +0900

    Add PressureVAS as a normal folder

---
+++ ios_app/PressureVAS/PressureVAS/PressureVASApp.swift
@@ -0,0 +1,17 @@
+//
+//  PressureVASApp.swift
+//  PressureVAS
+//
+//  Created by kizenMBP16 on 2025/12/12.
+//
+
+import SwiftUI
+
+@main
+struct PressureVASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
